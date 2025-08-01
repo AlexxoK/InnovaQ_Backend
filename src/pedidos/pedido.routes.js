f1657aec35523a93d3cce3f12197d44ab8e73ed3
pablocastillo4821@gmail.com <pablocastillo4821@gmail.com>
2025-08-01 15:05:48 -0600

    Cambios

--- src/pedidos/pedido.routes.js
+++ src/pedidos/pedido.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { check } from 'express-validator';
 import { validarCampos } from '../middlewares/validar-campos.js';
 import { validarJWT } from '../middlewares/validar-jwt.js';
-import { deletePedido, getPedidoById, getPedidos, savePedido, updatePedido, totalSumaAIngresos } from './pedido.controller.js';
+import { deletePedido, getPedidoById, getPedidosPorUsuario, getPedidos, savePedido, updatePedido, totalSumaAIngresos } from './pedido.controller.js';
 
 const router = Router();
 
@@ -16,6 +16,12 @@ router.get(
     '/',
     getPedidos
 );
+
+router.get(
+    '/pedidos-usuario',
+    validarJWT,
+    getPedidosPorUsuario
+)
 
 router.get(
     '/total-ingresos',
